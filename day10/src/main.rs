512e27061b158051107ddc5932eda123e229a89f
Krishna Kishore Shetty <kkshetty.work@pm.me>
2022-12-11 16:26:43 +0530

    Added code for Day 10 Part 2
    Signed-off-by: Krishna Kishore Shetty <kkshetty.work@pm.me>

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -23,12 +23,20 @@ fn main() {
 
     let mut int_signal_strengths: Vec<i32> = Vec::new();
 
+    // For Part 2 answer
+    let mut crt_display_row: String = String::new();
+
     debug!("Initial CPU State {:?}", cpu_state);
     for input_file_line in input_file_lines {
         if let Ok(current_line) = input_file_line {
             debug!("--");
             debug!("Main | Current CPU State before executor {:?}", cpu_state);
-            executor(&current_line, &mut cpu_state, &mut int_signal_strengths);
+            executor(
+                &current_line,
+                &mut cpu_state,
+                &mut int_signal_strengths,
+                &mut crt_display_row,
+            );
             debug!("Main | Current CPU State after executor {:?}", cpu_state);
         }
     }
@@ -37,9 +45,16 @@ fn main() {
         "Part 1 | What is the sum of these interesting signal strengths?\nAnswer: {}",
         int_signal_strengths.iter().sum::<i32>()
     );
+
+    println!("Part 2 | Pattern:\n{}", crt_display_row);
 }
 
-fn executor(input_line: &String, cpu_state: &mut CpuState, int_signal_strengths: &mut Vec<i32>) {
+fn executor(
+    input_line: &String,
+    cpu_state: &mut CpuState,
+    int_signal_strengths: &mut Vec<i32>,
+    crt_display_row: &mut String,
+) {
     trace!("Executing line: {}", input_line);
 
     let mut cycles_remaining = 0;
@@ -61,6 +76,27 @@ fn executor(input_line: &String, cpu_state: &mut CpuState, int_signal_strengths:
             cycles_remaining
         );
         cpu_state.cycle_count += 1;
+
+        // Code for part 2
+
+        warn!(
+            "CRT Drawing pixel at position {}. Sprite middle position at {} or {}",
+            cpu_state.cycle_count - 1,
+            cpu_state.register_x,
+            cpu_state.register_x + 40*((cpu_state.cycle_count as i32 - 1)/40)
+        );
+        if is_crt_drawing_sprite(cpu_state) {
+            warn!("Adding Lit Pixel for CPU Cycle {}", cpu_state.cycle_count);
+            crt_display_row.push('#');
+        } else {
+            warn!("Adding Dark Pixel for CPU Cycle {}", cpu_state.cycle_count);
+            crt_display_row.push('.');
+        }
+        //Add newline after every 40th character
+        if (cpu_state.cycle_count >= 40) && (cpu_state.cycle_count % 40 == 0) {
+            crt_display_row.push('\n');
+        }
+        debug!("CRT Draw String:\n{}", crt_display_row);
 
         /*
         Technically, the sequence for cycle counts at which need to record signal strebgths
@@ -110,4 +146,40 @@ fn executor(input_line: &String, cpu_state: &mut CpuState, int_signal_strengths:
 
         cycles_remaining -= 1;
     } //while loop
+}
+
+fn is_crt_drawing_sprite(cpu_state: &CpuState) -> bool {
+    // CRT always draws in pixel (current_cycle-1)
+    let current_draw_position: i32 = (cpu_state.cycle_count as i32) - 1;
+
+    // Since x value is negative
+    if cpu_state.register_x < 0 {
+        return false;
+    }
+
+        /*
+    On cycles 41 and above. CRT draws on rows other than first.
+    And register X only determines the "horizontal" position. 
+    Meaning it will never go above 40
+    If the register_X has a value of 35, but the CPU is on cycle 100
+    the CRT will be drawing on the third row of the 40x6 grid.
+    So the "effective" sprite position will be have to be determined
+    by adding a multiple of 40 to it.
+    */
+
+    let mut sprite_middle_position = cpu_state.register_x;
+
+    if current_draw_position > 40 {
+        sprite_middle_position += 40*(current_draw_position/40);
+    }
+
+    //Check if CRT is drawing one of the sprite pixels
+    if ((sprite_middle_position - 1) == current_draw_position)
+        || (sprite_middle_position == current_draw_position)
+        || ((sprite_middle_position + 1) == current_draw_position)
+    {
+        return true;
+    }
+
+    return false;
 }
